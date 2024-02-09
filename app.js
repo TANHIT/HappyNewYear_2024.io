c446ea8e20e39934f5c32e92fb5f35e88f70f8c9
TANHIT <20010842@st.phenikaa-uni.edu.vn>
2024-02-09 22:16:29 +0700

    own

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var fut = new Date("jan 12, 2023 13:29:00").getTime()
-let x = setInterval(function stime(){
-    var now = new Date().getTime()
-    var D = fut - now
-    var days = Math.floor(D/(1000*60*60*24))
-    var hours = Math.floor(D/(1000*60*60))
-    var minutes = Math.floor(D/(1000*60))
-    var seconds = Math.floor(D/(1000))
-    hours %=24
-    minutes %=60
-    seconds %=60
-    document.getElementById("days").innerText = days
-    document.getElementById("hours").innerText = hours
-    document.getElementById("minutes").innerText = minutes
-    document.getElementById("seconds").innerText = seconds
+var fut = new Date("feb 10, 2024 12:00:00 GMT+0700").getTime(); // Sửa thời gian bắt đầu
 
-    if(D<0){
+let x = setInterval(function stime() {
+    var now = new Date().getTime() + 12 * 60 * 60 * 1000; // Cộng thêm 7 giờ để chuyển múi giờ về Việt Nam
+    var D = fut - now;
+
+    if (D < 0) {
         clearInterval(x);
-        window.location.href="intro.html"
+        window.location.href = "intro.html";
+    } else {
+        var days = Math.floor(D / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((D % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((D % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((D % (1000 * 60)) / 1000);
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
     }
-}, 0.1);
+}, 1000);
